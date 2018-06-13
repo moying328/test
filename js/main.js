5f00dea26de6cb18de6eb215b69fadc31ad5f988
moying328 <2398437082@qq.com>
2018-06-13 16:38:30 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -11,6 +11,23 @@
                 document.documentElement.style.fontSize = (w/750)*100+"px"; 
         }
 			 /*rem的设置end*/
+             
+             /*商品加减start*/
+        function calculate(ob){
+            var opera = ob.name;
+            if(opera == '2'){
+                // console.log(ob.previousSibling.previousSibling);
+                var input = ob.previousSibling.previousSibling;
+                input.value = parseInt(input.value) + 1;
+            }else if(opera == '1'){
+                var input = ob.nextSibling.nextSibling;
+                input.value = parseInt(input.value) - 1;
+                if( input.value < 1 ){
+                    input.value = 1;
+                }
+            }
+        }
+        /*商品加减end*/
   
 
   
@@ -24,6 +41,7 @@ $(document).ready(function(){
     $("#top_tab h3").click(function(){
        $(this).addClass("active").siblings().removeClass("active");
         $("#tab_content .mytab").hide().eq($(this).index()-1).show();
+        myswiper.update()
         });
 
     $("#tab_content .hot").first().css("border-top","0");
@@ -69,5 +87,115 @@ $(document).ready(function(){
     }
     animate();
     /*进度条end*/
+
+
+/*手机号码和短信验证码start*/
+var code = ""; //接收验证码   
+var reg_phone = /1\d{10}/;
+function phonetest(){
+    var phone=$("#phone").val();//手机号码 
+    if(!reg_phone.test(phone)){   //验证手机是否符合格式
+        $(".prompt1").show();
+        return false;
+    } 
+}
+var reg_password=/^[a-z0-9]{6,16}$/;
+function passwordtest(){
+    var password=$("#mypassword").val();
+    if(!reg_password.test(password)){
+        $(".prompt3").show();
+    }
+}
+
+function radiotest1(){
+    var list= $('input:radio[name="radio"]:checked').val();
+            if(list==null){
+                $(".prompt4").show();
+                return false;
+            }
+            
+}
+
+function radiotest2(){
+    $(".magic-radio").click(function(){
+        var list= $('input:radio[name="radio"]:checked').val();
+            if(list!=null){
+                $(".prompt4").hide();
+                return true;
+            }
+    })
+    
+            
+}
+radiotest2();
+
+$('#code-btn').click(function(){
+  var count = 5;
+  phonetest();
+
+    //开始计时  
+    $("#code-btn").attr('disabled','disabled');  
+    $("#code-btn").html("重新获取" +"("+ count + "秒"+")");  
+    var timer = setInterval(function(){  
+      count--;
+      $("#code-btn").html("重新获取" +"("+ count + "秒"+")"); 
+      if (count==0) {
+        clearInterval(timer);
+        $("#code-btn").attr("disabled",false);//启用按钮  
+        $("#code-btn").html("获取短信验证码");
+        code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效 
+      }
+    },1000);
+
+    //向后台发送处理数据  
+    $.ajax({  
+        type: "POST", //用POST方式传输  
+        dataType: "text", //数据格式:JSON  
+        url: '', //目标地址  
+        data: "phone=" + phone + "&code=" + code,  
+        error: function (XMLHttpRequest, textStatus, errorThrown) { },  
+        success: function (msg){ }  
+    });
+});  
+
+$("#phone").blur(function(){
+var phone=$("#phone").val();//手机号码 
+ if(reg_phone.test(phone)){   //验证手机是否符合格式
+        $(".prompt1").hide();
+        return true;
+    } 
+});
+$("#checkCode").blur(function(){
+var phone=$("#phone").val();//手机号码 
+ if($("#checkCode").val()!=''){
+        $(".prompt2").hide();
+    }
+});
+$("#mypassword").blur(function(){
+var password=$("#mypassword").val();
+    if(reg_password.test(password)){
+        $(".prompt3").hide();
+    }
+})
+/*手机号码和短信验证码end*/
+
+/*新用户注册start*/
+$(".register_way .way").click(function(){
+    phonetest();
+    if($("#checkCode").val()==''){
+        $(".prompt2").show();
+    }
+    passwordtest();
+    radiotest1();
+})
+/*新用户注册end*/
+
+/*radio的显示start*/
+$('label').click(function(){
+    var radioId = $(this).attr('name');
+    $('label').removeClass('checked') && $(this).addClass('checked');
+    $('input[type="radio"]').removeAttr('checked') && $('#' + radioId).attr('checked', 'checked');
+  });    
+/*radio的显示end*/
 })
 
